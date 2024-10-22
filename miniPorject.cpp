084c6b6eff58c007506ada44c079f4a35ccdeaea
Dhanush KL <dhanuchandu1232@gmail.com>
2024-10-22 22:55:54 +0530

    task schedular

---
+++ miniPorject.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Process {
+    int id;               
+    int burstTime;       
+    int remainingTime;   
+};
+
+int main() {
+    int numProcesses, timeQuantum;
+
+    cout << "Enter the number of processes: ";
+    cin >> numProcesses;
+    cout << "Enter the time quantum: ";
+    cin >> timeQuantum;
+
+    queue<Process> processes;
+
+    for (int i = 1; i <= numProcesses; i++) {
+        Process p;
+        p.id = i;
+        cout << "Enter burst time for Process " << i << ": ";
+        cin >> p.burstTime;
+        p.remainingTime = p.burstTime;  
+        processes.push(p);  
+    }
+
+    int currentTime = 0;  
+
+   
+    while (!processes.empty()) {
+        Process currentProcess = processes.front(); 
+        processes.pop(); 
+
+        int executionTime = min(currentProcess.remainingTime, timeQuantum);
+
+        
+        currentProcess.remainingTime -= executionTime;
+        currentTime += executionTime; 
+
+        
+        cout << "Process " << currentProcess.id << " executed for " << executionTime << " time units. Remaining: " << currentProcess.remainingTime << endl;
+
+
+        if (currentProcess.remainingTime == 0) {
+            cout << "Process " << currentProcess.id << " finished at time " << currentTime << endl; 
+        } else {
+            processes.push(currentProcess); 
+        }
+    }
+
+    return 0;
+}
